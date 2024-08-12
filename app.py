5533dbadb77f3b38bc584c464554c07b82c090ff
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-08-13 06:57:52 +0900

    fix : req 버전 추가

--- app.py
+++ app.py
@@ -5,5 +5,3 @@ st.subheader("쪼랩 원데이클래스 두 번째 세션")
 st.success("streamlit으로 만든 페이지에 오신 것을 환영합니다!")
 st.image("data/images/zzolab_st.png", caption="Welcome to ZZOLAB") 
 
-st.warning("1. streamlit 기본(30분)\n2. 맞")
-st.link_button("streamlit cheat sheet", 'https://docs.streamlit.io/develop/quick-reference/cheat-sheet')
